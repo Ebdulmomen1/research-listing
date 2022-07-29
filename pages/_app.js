2fd1480b1b7a4b4356d55e5cd0fd9e10c51f8ba3
AbdulmomenAhmed <moamenahmad491@yahoo.com>
2022-07-29 16:58:12 +0300

    pass

--- pages/_app.js
+++ pages/_app.js
@@ -3,14 +3,15 @@ import "../styles/globals.css";
 // import PrivateRoute from "../components/PrivateRoutes";
 import Layout from "../components/Layout";
 import { appWithTranslation } from "next-i18next";
+import nextI18nConfig from "../next-i18next.config";
 
 function MyApp({ Component, pageProps }) {
   // const protectedRoutes = ["/panel"];
   return (
-    // <Layout>
-    <Component {...pageProps} />
-    // </Layout>
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
   );
 }
 
-export default appWithTranslation(MyApp);
+export default appWithTranslation(MyApp, nextI18nConfig);
